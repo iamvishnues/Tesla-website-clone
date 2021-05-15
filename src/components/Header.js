38044f758bda1637124148b84059fe881d6620c9
Vishnu ES <55495318+Vishnu-es@users.noreply.github.com>
2021-05-15 23:22:50 +0530

    Nav Bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
+import { selectCars } from "../features/car/CarSlice";
+import { useSelector } from "react-redux";
 
 function Header() {
+  const [BurgerStatus, setBurgerStatus] = useState(false);
+  const cars = useSelector(selectCars);
+
+  function setStatus() {
+    setBurgerStatus(!BurgerStatus);
+  }
   return (
     <Container>
       <a>
         <img src="images/logo.svg" alt="logo" />
       </a>
       <Menu>
-        <a href="#">Model S</a>
-        <a href="#">Model 3</a>
-        <a href="#">Model X</a>
-        <a href="#">Model Y</a>
-        <a href="#">Solar Roof</a>
-        <a href="#">Solar Panels</a>
+        {cars &&
+          cars.map((car, index) => (
+            <a key={index} href="#">
+              {car}
+            </a>
+          ))}
       </Menu>
       <RightMenu>
         <a href="#">Shop</a>
         <a href="#">Tesla Account</a>
-        <CustomMenu></CustomMenu>
+        <CustomMenu onClick={setStatus}></CustomMenu>
       </RightMenu>
+
+      {BurgerStatus ? (
+        <BurgerMenu show={BurgerStatus}>
+          <CloseWrapper>
+            <CustomClose onClick={setStatus}></CustomClose>
+          </CloseWrapper>
+          <li>
+            <a href="#">Model S</a>
+          </li>
+          <li>
+            <a href="#">Model 3</a>
+          </li>
+          <li>
+            <a href="#">Model X</a>
+          </li>
+          <li>
+            <a href="#">Model Y</a>
+          </li>
+          <li>
+            <a href="#">Model S</a>
+          </li>
+          <li>
+            <a href="#">Solar roof</a>
+          </li>
+          <li>
+            <a href="#">solar pannels</a>
+          </li>
+          <li>
+            <a href="#">More</a>
+          </li>
+          <li>
+            <a href="#">Tesla Account</a>
+          </li>
+        </BurgerMenu>
+      ) : null}
     </Container>
   );
 }
@@ -28,6 +72,7 @@ function Header() {
 export default Header;
 
 const Container = styled.div`
+  z-index: 1;
   min-height: 60px;
   position: fixed;
   display: flex;
@@ -49,9 +94,12 @@ const Menu = styled.div`
     padding: 0 10px;
     flex-wrap: no-wrap;
   }
-  @media (max-width: 750px) {
+  @media (max-width: 976px) {
     display: none;
   }
+  /* @media (max-width: 750px) {
+    display: none;
+  } */
 `;
 const RightMenu = styled.div`
   display: flex;
@@ -59,11 +107,43 @@ const RightMenu = styled.div`
   a {
     font-weight: 600;
     text-transform: uppercase;
-    padding: 0 10px;
+    padding: 0 5px;
     flex-wrap: no-wrap;
   }
   justify-content: center;
 `;
 const CustomMenu = styled(MenuIcon)`
   cursor: pointer;
+`;
+
+const BurgerMenu = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background: white;
+  width: 300px;
+  z-index: 100;
+  list-style: none;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid black;
+  }
+  a {
+    font-weight: 600;
+  }
+  /* transform: ${(props) =>
+    props.show ? "translateX(100%)" : "translateX(0)"}; */
+  transition: transform 0.2s ease-in-out;
+`;
+const CustomClose = styled(CloseIcon)`
+  cursor: pointer;
+`;
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
 `;
